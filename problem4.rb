3894b1cc0659077f9cd8d6355394a77489659f21
James Gromis <dagwoodsandwich56@gmail.com>
2019-04-05 20:31:36 +0200

    4 Balls Baby

--- problem4.rb
+++ problem4.rb
@@ -1,19 +1,80 @@
+require 'colorize'
 hardness = rand(1..128)
-128.times do |i|
+hardness2 = rand(1..256)
+hardness3 = rand(1..384)
+hardness4 = rand(1..512)
+  hardnesscompletion = 0
+512.times do |i|
   floornumber = i + 1
   puts "Floor Nr. #{floornumber}"
   hardnesslvl = floornumber
   if hardness > hardnesslvl then
-    puts "Ball has been broken"
+    puts "Ball 1 has not been broken(Keep going)"
   else
   end
+
+  if hardness2 > hardnesslvl then
+    puts "Ball 2 has not been broken(Keep going)"
+  else
+  end
+
+  if hardness3 > hardnesslvl then
+    puts "Ball 3 has not been broken(Keep going)"
+  else
+  end
+
+  if hardness4 > hardnesslvl then
+    puts "Ball 4 has not been broken(Keep going)"
+  else
+  end
+
   if hardness < hardnesslvl then
-    puts "Ball has not been broken(Keep going)"
+puts "Ball 1 has been broken"
   else
   end
+
+  if hardness2 < hardnesslvl then
+    puts "Ball 2 has been broken"
+  else
+  end
+
+  if hardness3 < hardnesslvl then
+    puts "Ball 3 has been broken"
+  else
+  end
+
+  if hardness4 < hardnesslvl then
+    puts "Ball 4 has been broken"
+  else
+  end
+
   if hardness == hardnesslvl then
-    puts "Hardness found!"
-    gets
+    puts "Hardness found! (Ball 1)".yellow
+    hardnesscompletion += 1
+  else
+  end
+
+  if hardness2 == hardnesslvl then
+      puts "Hardness found! (Ball 2)".yellow
+          hardnesscompletion += 1
+  else
+  end
+
+  if hardness3 == hardnesslvl then
+    puts "Hardness found! (Ball 3)".yellow
+    hardnesscompletion += 1
+  else
+  end
+
+  if hardness4 == hardnesslvl then
+    puts "Hardness found! (Ball 4)".yellow
+    hardnesscompletion += 1
+  else
+  end
+
+  if hardnesscompletion == 4 then
+  puts "All Hardness Values found".yellow
+  gets
   else
   end
 end
