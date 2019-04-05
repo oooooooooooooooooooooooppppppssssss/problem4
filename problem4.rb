80cfcab7f1b0a522ea8bd95d66e20482b83482c3
James Gromis <dagwoodsandwich56@gmail.com>
2019-04-05 20:41:29 +0200

    Displays Floor numbers at which each ball broke

--- problem4.rb
+++ problem4.rb
@@ -3,6 +3,10 @@ hardness = rand(1..128)
 hardness2 = rand(1..256)
 hardness3 = rand(1..384)
 hardness4 = rand(1..512)
+hardnessfloor1 = 0
+hardnessfloor2 = 0
+hardnessfloor3 = 0
+hardnessfloor4 = 0
   hardnesscompletion = 0
 512.times do |i|
   floornumber = i + 1
@@ -51,29 +55,37 @@ puts "Ball 1 has been broken"
   if hardness == hardnesslvl then
     puts "Hardness found! (Ball 1)".yellow
     hardnesscompletion += 1
+    hardnessfloor1 = hardnesslvl
   else
   end
 
   if hardness2 == hardnesslvl then
       puts "Hardness found! (Ball 2)".yellow
           hardnesscompletion += 1
+            hardnessfloor2 = hardnesslvl
   else
   end
 
   if hardness3 == hardnesslvl then
     puts "Hardness found! (Ball 3)".yellow
     hardnesscompletion += 1
+      hardnessfloor3 = hardnesslvl
   else
   end
 
   if hardness4 == hardnesslvl then
     puts "Hardness found! (Ball 4)".yellow
     hardnesscompletion += 1
+      hardnessfloor4 = hardnesslvl
   else
   end
 
   if hardnesscompletion == 4 then
   puts "All Hardness Values found".yellow
+  puts "Ball 1 Broke on Floor Nr.#{hardnessfloor1}".yellow
+  puts "Ball 2 Broke on Floor Nr.#{hardnessfloor2}".yellow
+  puts "Ball 3 Broke on Floor Nr.#{hardnessfloor3}".yellow
+  puts "Ball 4 Broke on Floor Nr.#{hardnessfloor4}".yellow
   gets
   else
   end
